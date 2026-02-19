b1f182341a71543af689e648bbd122d95d99d93d
Artem Stadnik <stadnikartem1709@gmail.com>
2026-02-19 15:57:56 +0200

    fixed

--- src/pages/PaperPage.tsx
+++ src/pages/PaperPage.tsx
@@ -1,11 +1,22 @@
 import { Catalog } from '@/components/Catalog/Catalog';
 import { getPaperBooks } from '@/services/booksAPI';
 import { useFetchBooks } from '@/hooks/useFetchBooks';
+import { TYPOGRAPHY } from '@/constants/typography';
 
 export const PaperPage = () => {
   const { books, error, isLoading } = useFetchBooks(getPaperBooks);
 
-  if (error) return <div>{error}</div>;
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (isLoading) {
+    return (
+      <div className="w-full min-h-[60vh] flex items-center justify-center">
+        <p className={TYPOGRAPHY.h3}>Loading...</p>
+      </div>
+    );
+  }
 
   return (
     <Catalog
